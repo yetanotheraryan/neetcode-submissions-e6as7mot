3ba0773907450d72ef8dc5e88a2d24aae260cdf9
Aryan Tiwari <53210551+yetanotheraryan@users.noreply.github.com>
2026-03-31 23:06:35 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.go
@@ -0,0 +1,17 @@
+func search(nums []int, target int) int {
+    l := 0
+    r := len(nums) - 1
+
+    for l <= r {
+        mid := (l + r)/ 2
+        if nums[mid] > target {
+            r = mid - 1
+        } else if nums[mid] < target  {
+            l = mid + 1
+        } else {
+            return mid
+        }
+    }
+    return -1
+    
+}
